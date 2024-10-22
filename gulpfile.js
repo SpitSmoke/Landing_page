cb92b1c1a930a60792f225da82eff68c389906a3
SpitSmoke <jmfelicio.sp@gmail.com>
2024-10-21 22:22:57 -0300

    Vercel deploying

--- gulpfile.js
+++ gulpfile.js
@@ -4,25 +4,29 @@ const imagemin = require('gulp-imagemin');
 const uglify = require('gulp-uglify');
 
 function scripts() {
-    return gulp.src('./src/scripts/*.js')
-    .pipe(uglify())
-    .pipe(gulp.dest('./dist/js'))
+return gulp
+  .src("./src/scripts/*.js")
+  .pipe(uglify())
+  .pipe(gulp.dest("./public/js")); 
 }
 
 function styles() {
-    return gulp.src('./src/styles/*.scss')
-    .pipe(sass({ outputStyle: 'compressed' }))
-    .pipe(gulp.dest('./dist/css'))
+return gulp
+  .src("./src/styles/*.scss")
+  .pipe(sass({ outputStyle: "compressed" }))
+  .pipe(gulp.dest("./public/css")); 
 }
 
 function images() {
-    return gulp.src('./src/images/**/*')
-    .pipe(imagemin())
-    .pipe(gulp.dest('./dist/images'));
+return gulp
+  .src("./src/images/**/*")
+  .pipe(imagemin())
+  .pipe(gulp.dest("./public/images")); 
 }
 
+
 exports.default = gulp.parallel(styles, images, scripts);
-exports.watch = function() {
-    gulp.watch('./src/styles/*.scss', gulp.parallel(styles))
-    gulp.watch('./src/scripts/*.js', gulp.parallel(scripts))
-}
+exports.watch = function () {
+  gulp.watch("./src/styles/*.scss", gulp.parallel(styles));
+  gulp.watch("./src/scripts/*.js", gulp.parallel(scripts));
+};
